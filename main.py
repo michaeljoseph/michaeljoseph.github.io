e5af1a64b135b8b8a483bbed9111498b8949ac3a
Michael Joseph <michaeljoseph+github@gmail.com>
2022-05-25 13:18:03 +0200

    Add salutations and use vanilla requests

--- main.py
+++ main.py
@@ -5,18 +5,11 @@ import json
 import yaml
 from pathlib import Path
 
-from requests_cache import CachedSession
+from itertools import product
+import json
+import random
 
-from resume.badges import devicons_badges
-from resume.salutations import typed_salutations
-
-class HttpClient(CachedSession):
-    def __init__(self, expire_after=None):
-        CachedSession.__init__(self,
-            cache_name='mycull.dev',
-            use_temp=True,
-            expire_after=timedelta(minutes=30) if expire_after is None else expire_after,
-        )
+import requests
 
 NLJ = partial('\n'.join)
 JSDELIVR_DEVICONS = "https://cdn.jsdelivr.net/gh/devicons/devicon@master/icons"
@@ -32,7 +25,7 @@ DEVICONS_JSON = {
         f"{JSDELIVR_DEVICONS}/{icon['name']}/{icon['name']}-{icon['versions']['svg'][0]}.svg",
         icon['color'],
     )
-    for icon in HttpClient().get(DEVICONS_GITHUB).json()
+    for icon in requests.get(DEVICONS_GITHUB).json()
 }
 
 def load_resume(format='yaml', name='resume'):
@@ -84,8 +77,64 @@ def devicons_badges(company=None):
 
     return badges
 
+def salutation_permutations():
+    """Say 'Hello, my name is' in a bunch of ways"""
+    hellojis = '👋 🌊 🙋 🖖'.split()
+    # https://www.babbel.com/en/magazine/how-to-say-hello-in-10-different-languages
+    hello = """
+        hi yo oi
+        sup hey olá hoi hai hej
+        ciao hiya aweh hola haai molo 
+        heita hello salut ahlan 
+        howzit hoesit dumela halløj goddag 
+        sawubona 
+    """.split()
+    hello.extend(['whakind eksê', 'hoe lyk it'])
+    hello = 'hi yo sup hiya hey haai aweh hola molo haai'.split()
+    # my_names_are = ['my name is', 'my naam is', 'igama lami ngu']
+    popeyes = ["i'm ", "ek is ", "ngingu", "ke "]
+    amongst_our_names = "michael michel mikhail mihály mícheál michele michal miguel mihail michiel mikel mihangel mícheál mikkel".split()
+
+    # https://smodin.io/translate-one-text-into-multiple-languages
+
+    salutation_permutations = list(product(
+        hellojis, hello, popeyes, amongst_our_names
+    ))
+    random.shuffle(salutation_permutations)
+
+    salutations =  [ 
+        f'🤓{emoji}{hello}, {my_name_is}{me}'
+        for emoji, hello, my_name_is, me in salutation_permutations
+    ]
+    return salutations
+
+TYPED_JS = "https://cdn.jsdelivr.net/npm/typed.js@2.0.12"
+TYPED_TEMPLATE = """
+<script src="{js_url}"></script>
+<script>
+    var typed = new Typed('{dom_element}', {options});
+</script>
+"""
+
+def typed_js(dom_element, things_to_type):
+    options = json.dumps(dict(
+        startDelay=3000,
+        smartDelete=True,
+        showCursor=False,
+        typeSpeed=50,
+        strings=things_to_type,
+    ))
+    return TYPED_TEMPLATE.format(
+        js_url=TYPED_JS,
+        dom_element=dom_element,
+        options=options
+    )
+
 def skills_badges(company=None):
     return NLJ(devicons_badges(company))
+
+def typed_salutations():
+    return typed_js('h1', salutation_permutations())
 
 def define_env(env):
     env.variables['skills_badges'] = skills_badges
